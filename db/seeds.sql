6e5e7841cbbd796dac8a307a3abcc0aabcdfa1f5
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-02-06 16:10:29 -0500

    seeds and schema up and running

---
+++ db/seeds.sql
@@ -0,0 +1,36 @@
+
+INSERT INTO department (dept_name)
+VALUES  ('Front Office'),
+        ('Back Office'),
+        ('Side Office'),
+        ('Help Desk'),
+        ('Information Technology');
+
+INSERT INTO roles (title, salary)
+VALUES  ('Sales Director', 100000),
+        ('Sales Team', 20000),
+        ('Legal Analyst', 55000),
+        ('Bus. Strategy Director', 60000),
+        ('Softball Coach', 35000),
+        ('IT Analyst', 65000),
+        ('Helpdesk Lead', 75000),
+        ('Software Developer', 90000),
+        ('Lead Software Developer', 110000),
+        ('Software Architect', 165000);
+
+Insert INTO employee (first_name, last_name)
+VALUES ('Bob', 'Evans'),
+       ('Michael', 'Jordan'),
+       ('Tiger', 'Woods'),
+       ('Jack', 'Nicklaus'),
+       ('Steve', 'Jobs'),
+       ('Enzo', 'Ferarri'),
+       ('Henry', 'Ford'),
+       ('Benjamin', 'Franklin'),
+       ('Marcus', 'Aurelius');
+
+      
+      
+
+
+
